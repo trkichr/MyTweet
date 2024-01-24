7a0b55fe47843bd7b24dc30acaa28da42cec70fb
trkichr <trkichr@gmail.com>
2024-01-25 00:42:14 +0900

    p71_つぶやきを投稿する処理と作成する

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
+    ->name('tweet.index');
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+    ->name('tweet.create');
