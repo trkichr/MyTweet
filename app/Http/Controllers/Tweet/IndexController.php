9c1b0364831061df8c1ab2bbdafbb4b6de96ddc2
trkichr <trkichr@gmail.com>
2024-01-25 00:35:57 +0900

    p68_tweet内容の表示

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -15,8 +16,8 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $tweets = Tweet::all();
         return view('tweet.index')
-                    ->with('name', 'laravel')
-                    ->with('version', '8');
+            ->with('tweets', $tweets);
     }
 }
