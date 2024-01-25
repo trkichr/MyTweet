a9cde3ffa8bf384674ad6f18daebcc61f4d88667
trkichr <trkichr@gmail.com>
2024-01-25 17:27:17 +0900

    p90_編集内容の更新処理

--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -12,6 +12,9 @@
 <div>
     <a href="{{ route('tweet.index') }}">< 戻る</a>
     <p>投稿フォーム</p>
+    @if (session('feedback.success'))
+    <p style="color: green">{{ session('feedback.success') }}</p>
+    @endif
     <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
         @method('PUT')
         @csrf
