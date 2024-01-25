70c34beff8ca6493d2f6a22033080b501f3770eb
trkichr <trkichr@gmail.com>
2024-01-25 17:15:05 +0900

    p83_コントローラの作成

---
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Tweet\Update;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $tweetId = (int) $request->route('tweetId');
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
+        dd($tweet);
+    }
+}
