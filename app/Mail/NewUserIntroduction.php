10d318b2bdda1c2a78e5b8275dba0deed2e289c6
trkichr <trkichr@gmail.com>
2024-01-29 23:00:00 +0900

    p202_Queueを使ってメールを送信する

--- app/Mail/NewUserIntroduction.php
+++ app/Mail/NewUserIntroduction.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NewUserIntroduction extends Mailable
+class NewUserIntroduction extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
