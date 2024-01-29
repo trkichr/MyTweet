4edd2ecfce369f2d979a1415238ca98f6dd97235
trkichr <trkichr@gmail.com>
2024-01-30 00:34:30 +0900

    p212_前日のつぶやきのハイライトをメールで送る

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,19 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // 毎分
-        $schedule->command('sample-command')->everyMinute()
-            ->emailOutputTo('info@example.com');
-        // 毎時
-        $schedule->command('sample-command')->hourly();
-        // 毎時8分
-        $schedule->command('sample-command')->hourlyAt(8);
-        // 毎日
-        $schedule->command('sample-command')->daily();
-        // 毎日13時
-        $schedule->command('sample-command')->dailyAt('13:00');
-        // 毎日3:15(cron表記)
-        $schedule->command('sample-command')->cron('15 3 * * *');
+        $schedule->command('mail:send-daily-tweet-count-mail')
+            ->dailyAt('11:00');
     }
 
     /**
