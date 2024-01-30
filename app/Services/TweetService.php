a06ab99d65b91f68f3453f19d047a1e6993cc562
trkichr <trkichr@gmail.com>
2024-01-30 17:21:23 +0900

    p228_つぶやき一覧に画像を表示する

--- app/Services/TweetService.php
+++ app/Services/TweetService.php
@@ -9,7 +9,9 @@ class TweetService
 {
     public function getTweets()
     {
-        return Tweet::orderBy('created_at', 'DESC')->get();
+        return Tweet::with('images')
+                    ->orderBy('created_at', 'DESC')
+                    ->get();
     }
 
     // 自分のtweetかどうかをチェックするメソッド
